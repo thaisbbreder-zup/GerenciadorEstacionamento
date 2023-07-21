59dbe4e8169fed3f2ffd88e3c02874d6f2506817
Thaís Breder <thaisbragabreder@gmail.com>
2023-07-21 18:42:13 -0300

    :tada: sparkles: commit inicial

--- src/main/java/org/example/Conexao.java
+++ src/main/java/org/example/Conexao.java
@@ -8,28 +8,5 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Conexao {
-    public static Connection getConnection() {
-        try {
-            Properties propriedades = carregarPropriedades();
-            String url = propriedades.getProperty("dburl");
-            Connection conexao = DriverManager.getConnection(url, propriedades);
-            System.out.println("Conexão realizada com sucesso!");
-            return conexao;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 
-    private static Properties carregarPropriedades() {
-        try (FileInputStream inputStream = new FileInputStream("src/main/resources/db.properties")) {
-            Properties propriedades = new Properties();
-            propriedades.load(inputStream);
-            return propriedades;
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
