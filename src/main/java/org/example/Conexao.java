e417846d95aac03767fc4bf803cb8de371412ae4
Thaís Breder <thaisbragabreder@gmail.com>
2023-07-21 15:33:43 -0300

    :data: init: commit inicial

---
+++ src/main/java/org/example/Conexao.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Conexao {
+    public static Connection getConnection() {
+        try {
+            Properties propriedades = carregarPropriedades();
+            String url = propriedades.getProperty("dburl");
+            Connection conexao = DriverManager.getConnection(url, propriedades);
+            System.out.println("Conexão realizada com sucesso!");
+            return conexao;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static Properties carregarPropriedades() {
+        try (FileInputStream inputStream = new FileInputStream("src/main/resources/db.properties")) {
+            Properties propriedades = new Properties();
+            propriedades.load(inputStream);
+            return propriedades;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
